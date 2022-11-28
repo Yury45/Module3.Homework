9713178bf372f10d138f3d3fa80ef1453d9b00c0
Yury <zm.ptz.ru2010@yandex.ru>
2022-11-28 21:34:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Questionnaire questionnaire = new Questionnaire();
+
+            Ask("Как вас зовут?");
+            questionnaire.Name = GetAnswer();
+            Ask("Сколько вам лет?");
+            questionnaire.Age = GetAnswer();
+            Ask("Напишите дату вашего рождения:");
+            questionnaire.Birthday = GetAnswer();
+
+            questionnaire.Print();
+            Console.ReadKey();
+        }
+
+        static void Ask(string question)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(question);
+        }
+
+        static string GetAnswer()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            return Console.ReadLine();
+        }
+    }
+}
---
+++ Questionnaire.cs
@@ -0,0 +1,68 @@
+﻿namespace Module3
+{
+    internal class Questionnaire
+    {
+        private string name;
+
+        internal string Name
+        {
+            private get { return name; }
+            set { name = value; }
+        }
+
+        private byte age;
+
+        internal string Age
+        {
+            private get { return age.ToString(); }
+            set
+            {
+                byte tempAge;
+                Byte.TryParse(value, out tempAge);
+
+                while ( tempAge <= 14 || tempAge >= 150)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nНекорректный возраст!\nВведите ваш возраст еще раз...");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Byte.TryParse(Console.ReadLine(), out tempAge);
+                }
+
+                age = tempAge;
+            }
+        }
+
+        private DateTime birthday;
+
+        internal string Birthday
+        {
+            private get { return birthday.ToString();}
+            set
+            {
+                DateTime tempBirthday;
+                DateTime.TryParse(value, out tempBirthday);
+
+                while ( tempBirthday <= DateTime.MinValue || tempBirthday >= DateTime.Now)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nНекорректная дата рождения!\nВведите дату рождения еще раз...");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    DateTime.TryParse(Console.ReadLine(), out tempBirthday);
+                }
+
+                birthday = tempBirthday;
+
+            }
+        }
+
+        internal void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen; 
+            Console.WriteLine($"");
+            Console.WriteLine($"Your name is {Name}");
+            Console.WriteLine($"Your age is {Age}");
+            Console.WriteLine($"You was born on {Birthday}");
+
+        }
+    }
+}
